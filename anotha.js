6b4e24927f5077f5d4cc4e0db46bd3eb05ae745c
leeweisberger <lee@airplane.dev>
2022-02-17 10:18:02 -0500

    initial

---
+++ anotha.js
@@ -0,0 +1,12 @@
+// Linked to https://app.airplane.so:5000/t/anotha [do not edit this line]
+
+// Put the main logic of the task in this function.
+export default async function(params) {
+  console.log('parameters:', params);
+  // You can return data to show outputs to users.
+  // Outputs documentation: https://docs.airplane.dev/tasks/outputs
+  return [
+    {element: 'hydrogen!!!e', weight: 1.008},
+    {element: 'helium!!!e', weight: 4.0026},
+  ];
+}
--- anotha_one copy.js
+++
@@ -1,6 +0,0 @@
-
-// Linked to https://app.airplane.so:5000/t/anotha_one2 [do not edit this line]
-
-export default async function(params) {
-  console.log('anotha:', params);
-}
